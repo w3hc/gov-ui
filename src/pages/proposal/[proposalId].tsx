2b98cb28fd8e83de8b62f0141702a18d9bdeee25
julienbrg <julien@strat.cc>
2024-04-15 18:23:20 +0200

    activate voting

--- src/pages/proposal/[proposalId].tsx
+++ src/pages/proposal/[proposalId].tsx
@@ -6,6 +6,7 @@ import { useWeb3ModalProvider, useWeb3ModalAccount } from '@web3modal/ethers/rea
 import { LinkComponent } from '../../components/layout/LinkComponent'
 import { Head } from '../../components/layout/Head'
 import govContract from '../../utils/Gov.json'
+import nftContract from '../../utils/NFT.json'
 import { ethers } from 'ethers'
 import { HeadingComponent } from '../../components/layout/HeadingComponent'
 import { AddIcon } from '@chakra-ui/icons'
@@ -77,77 +78,58 @@ export default function Home() {
   }
 
   const getProposalData = async () => {
-    // const block = await provider.getBlockNumber()
-    if (initialized) {
-      console.log('already initialized')
-      setIsLoading(false)
-      return
-    }
-    setIsLoading(true)
-
-    console.log('proposalId:', proposalId)
-
-    const gov = new ethers.Contract(govContract.address, govContract.abi, customProvider)
-
-    const proposalCreatedBlocks = await gov.getProposalCreatedBlocks()
-    console.log('proposalCreatedBlocks', proposalCreatedBlocks)
-
-    let block
-    for (let i = 0; i < proposalCreatedBlocks.length; i++) {
-      console.log('iteration:', i)
-
-      console.log('proposalCreatedBlocks[i]:', proposalCreatedBlocks[i])
-      const proposals: any = await gov.queryFilter('ProposalCreated', block)
-      console.log('proposals:', proposals)
-
-      //   console.log('proposals[0].args[0]', proposals[0].args[0])
-
-      console.log('String(proposals[i].args[0])', String(proposals[i].args[0]))
-      console.log('proposalId:', proposalId)
-
-      if (String(proposals[i].args[0]) === proposalId) {
-        console.log('yo')
-        console.log('proposals[0].args[0]', proposals[0].args[0])
-
-        setTitle(proposals[i].args[8].substring(proposals[i].args[8] == '#' ? 2 : 2, proposals[i].args[8].indexOf('\n')))
-        setDescription(proposals[i].args[8].substring(proposals[i].args[8].indexOf('\n')))
-        setUri(proposals[i].args[8].substring(proposals[i].args[8].indexOf('(') + 1, proposals[i].args[8].indexOf(')')))
-        setCalldatas(proposals[i].args[5])
-        await getState(proposalId)
-        await getCurrentVotes(proposalId)
-        setTargets(proposals[i].args[2][0])
-        setValues(proposals[i].args[3][0])
-        setCalldatas(proposals[i].args[5][0])
-        setRawDescription(proposals[i].args[8])
-      }
-    }
-
-    const proposal: any = await gov.queryFilter('ProposalCreated' as any, 5065858, block) // TODO: fix type casting
+    // setIsLoading(true)
 
     try {
-      //   let i: number = 0
-      //   if (proposals[0].args != undefined) {
-      //     for (i; i < Number(proposals.length); i++) {
-      //       const id = String(proposals[i].args?.proposalId)
-      //       if (id == proposalId) {
-      //         setTitle(proposals[i].args[8].substring(proposals[i].args[8][0] == '#' ? 2 : 0, proposals[i].args[8].indexOf('\n')))
-      //         setDescription(proposals[i].args[8].substring(proposals[i].args[8].indexOf('\n')))
-      //         setUri(proposals[i].args[8].substring(proposals[i].args[8].indexOf('(') + 1, proposals[i].args[8].indexOf(')')))
-      //         setCalldatas(proposals[0].args[5])
-      //         await getState(proposalId)
-      //         await getCurrentVotes(proposalId)
-      //         setTargets(proposals[i].args[2][0])
-      //         setValues(proposals[i].args[3][0])
-      //         setCalldatas(proposals[i].args[5][0])
-      //         setRawDescription(proposals[i].args[8])
-      //         setInitialized(true)
-      //       }
-      //     }
-      //   }
+      if (initialized) {
+        console.log('already initialized')
+        // setIsLoading(false)
+        return
+      }
+
+      console.log('proposalId:', proposalId)
+
+      const gov = new ethers.Contract(govContract.address, govContract.abi, customProvider)
+
+      const proposalCreatedBlocks = await gov.getProposalCreatedBlocks()
+      console.log('proposalCreatedBlocks', proposalCreatedBlocks)
+
+      let block
+      for (let i = 0; i < proposalCreatedBlocks.length; i++) {
+        console.log('iteration:', i)
+
+        console.log('proposalCreatedBlocks[i]:', proposalCreatedBlocks[i])
+        const proposals: any = await gov.queryFilter('ProposalCreated', block)
+        console.log('proposals:', proposals)
+
+        //   console.log('proposals[0].args[0]', proposals[0].args[0])
+
+        console.log('String(proposals[i].args[0])', String(proposals[i].args[0]))
+        console.log('proposalId:', proposalId)
+
+        if (String(proposals[i].args[0]) === proposalId) {
+          console.log('yo')
+          console.log('proposals[0].args[0]', proposals[0].args[0])
+
+          setTitle(proposals[i].args[8].substring(proposals[i].args[8] == '#' ? 2 : 2, proposals[i].args[8].indexOf('\n')))
+          setDescription(proposals[i].args[8].substring(proposals[i].args[8].indexOf('\n')))
+          setUri(proposals[i].args[8].substring(proposals[i].args[8].indexOf('(') + 1, proposals[i].args[8].indexOf(')')))
+          setCalldatas(proposals[i].args[5])
+          await getState(proposalId)
+          await getCurrentVotes(proposalId)
+          setTargets(proposals[i].args[2][0])
+          setValues(proposals[i].args[3][0])
+          setCalldatas(proposals[i].args[5][0])
+          setRawDescription(proposals[i].args[8])
+        }
+      }
+
       setInitialized(true)
+      // setIsLoading(true)
     } catch (error) {
       console.error('error:', error)
       setInitialized(true)
+      // setIsLoading(true)
     }
   }
 
@@ -159,161 +141,209 @@ export default function Home() {
   }, [proposalId])
 
   const hasDelegated = async () => {
-    // const gov = new ethers.Contract(GOV_CONTRACT_ADDRESS, GOV_CONTRACT_ABI, signer)
-    // const delegateTo = await signer?.getAddress()
-    // const nftAddress = await gov.token()
-    // const nft = new ethers.Contract(nftAddress, nftAbi, signer)
-    // const delegate = await nft.delegates(await signer?.getAddress())
-    // if (delegate === delegateTo) {
-    //   return true
-    // }
+    console.log('hasDelegated start')
+    let signer: any
+    if (provider) {
+      // setIsLoading(true)
+      const ethersProvider = new BrowserProvider(provider)
+      signer = await ethersProvider.getSigner()
+      const delegateTo = await signer?.getAddress()
+      const nft = new ethers.Contract(nftContract.address, nftContract.abi, signer)
+      const delegate = await nft.delegates(await signer?.getAddress())
+      if (delegate === delegateTo) {
+        return true
+      }
+      console.log('hasDelegated done')
+    }
   }
 
   const delegateToMyself = async () => {
-    // if ((await hasDelegated()) === true) {
-    //   return true
-    // } else {
-    //   const delegateTo = await signer?.getAddress()
-    //   const gov = new ethers.Contract(GOV_CONTRACT_ADDRESS, GOV_CONTRACT_ABI, signer)
-    //   const nftAddress = await gov.token()
-    //   const nft = new ethers.Contract(nftAddress, nftAbi, signer)
-    //   const delegate = await nft.delegate(delegateTo)
-    //   const receippt = await delegate.wait(1)
-    //   return true
-    // }
+    console.log('delegateToMyself start')
+
+    if ((await hasDelegated()) === true) {
+      return true
+    } else {
+      let signer: any
+      if (provider) {
+        // setIsLoading(true)
+        const ethersProvider = new BrowserProvider(provider)
+        signer = await ethersProvider.getSigner()
+        const delegateTo = await signer?.getAddress()
+        const nft = new ethers.Contract(nftContract.address, nftContract.abi, signer)
+        const delegate = await nft.delegate(delegateTo)
+        const receipt = await delegate.wait(1)
+        // setIsLoading(false)
+        return true
+      }
+    }
+    console.log('delegateToMyself done')
   }
 
   const voteYes = async () => {
     // https://docs.openzeppelin.com/contracts/4.x/api/governance#IGovernor-COUNTING_MODE--
     // 0 = Against, 1 = For, 2 = Abstain
-    // if (signer === undefined) {
-    //   toast({
-    //     title: 'Disconnected',
-    //     position: 'bottom',
-    //     description: 'Please connect your wallet first.',
-    //     status: 'info',
-    //     variant: 'subtle',
-    //     duration: 2000,
-    //     isClosable: true,
-    //   })
-    //   return
-    // }
-    // try {
-    //   const delegate = await delegateToMyself()
-    // } catch (e) {
-    //   console.log('delegate error:', e)
-    // }
-    // try {
-    //   console.log('voting...')
-    //   const gov = new ethers.Contract(GOV_CONTRACT_ADDRESS, GOV_CONTRACT_ABI, signer)
-    //   await gov.castVote(router.query.proposalId, 1)
-    //   await getCurrentVotes(router.query.proposalId)
-    //   setForVotes(forVotes + 1)
-    //   await getState(proposalId)
-    //   toast({
-    //     title: 'Voted!',
-    //     position: 'bottom',
-    //     description: 'Thank you for voting this proposal.',
-    //     status: 'success',
-    //     variant: 'subtle',
-    //     duration: 5000,
-    //     isClosable: true,
-    //   })
-    // } catch (e: any) {
-    //   console.log('vote error:', e)
-    //   toast({
-    //     title: 'Error',
-    //     position: 'bottom',
-    //     description: "You can't vote twice",
-    //     status: 'error',
-    //     variant: 'subtle',
-    //     duration: 3000,
-    //     isClosable: true,
-    //   })
-    // }
+
+    let signer
+
+    try {
+      delegateToMyself()
+    } catch (e) {
+      console.log('delegate error:', e)
+    }
+    if (provider) {
+      // setIsLoading(true)
+      const ethersProvider = new BrowserProvider(provider)
+      signer = await ethersProvider.getSigner()
+
+      console.log('signer:', signer)
+
+      try {
+        console.log('voting Yes...')
+
+        const gov = new ethers.Contract(govContract.address, govContract.abi, signer)
+        console.log('gov:', gov)
+        await gov.castVote(router.query.proposalId, 1)
+        await getCurrentVotes(router.query.proposalId)
+        setForVotes(forVotes + 1)
+        await getState(proposalId)
+        toast({
+          title: 'Voted!',
+          position: 'bottom',
+          description: 'Thank you for voting this proposal.',
+          status: 'success',
+          variant: 'subtle',
+          duration: 5000,
+          isClosable: true,
+        })
+      } catch (e: any) {
+        console.log('vote error:', e)
+        toast({
+          title: 'Error',
+          position: 'bottom',
+          description: "You can't vote twice",
+          status: 'error',
+          variant: 'subtle',
+          duration: 3000,
+          isClosable: true,
+        })
+      }
+      // setIsLoading(false)
+    } else {
+      console.log('no provider')
+      toast({
+        title: 'Disconnected',
+        position: 'bottom',
+        description: 'Please connect your wallet first.',
+        status: 'info',
+        variant: 'subtle',
+        duration: 2000,
+        isClosable: true,
+      })
+      // setIsLoading(false)
+      return
+    }
   }
 
   const voteNo = async () => {
-    console.log('voting...')
+    console.log('voting No...')
 
-    // if (signer === undefined) {
-    //   toast({
-    //     title: 'Disconnected',
-    //     position: 'bottom',
-    //     description: 'Please connect your wallet first.',
-    //     status: 'info',
-    //     variant: 'subtle',
-    //     duration: 2000,
-    //     isClosable: true,
-    //   })
-    //   return
-    // }
+    // https://docs.openzeppelin.com/contracts/4.x/api/governance#IGovernor-COUNTING_MODE--
+    // 0 = Against, 1 = For, 2 = Abstain
 
-    // try {
-    //   const delegate = await delegateToMyself()
-    // } catch (e) {
-    //   console.log('delegate error:', e)
-    // }
-    // try {
-    //   const gov = new ethers.Contract(GOV_CONTRACT_ADDRESS, GOV_CONTRACT_ABI, signer)
-    //   await gov.castVote(router.query.proposalId, 0)
-    //   await getCurrentVotes(router.query.proposalId)
-    //   setAgainstVotes(againstVotes + 1)
-    //   await getState(proposalId)
-    //   toast({
-    //     title: 'Voted!',
-    //     position: 'bottom',
-    //     description: 'Thank you for voting this proposal.',
-    //     status: 'success',
-    //     variant: 'subtle',
-    //     duration: 2000,
-    //     isClosable: true,
-    //   })
-    // } catch (e: any) {
-    //   console.log('vote error:', e)
+    let signer
+    if (provider) {
+      // setIsLoading(true)
+      const ethersProvider = new BrowserProvider(provider)
+      signer = await ethersProvider.getSigner()
 
-    //   toast({
-    //     title: 'Error',
-    //     position: 'bottom',
-    //     description: "You can't vote twice",
-    //     status: 'error',
-    //     variant: 'subtle',
-    //     duration: 3000,
-    //     isClosable: true,
-    //   })
-    // }
+      console.log('signer:', signer)
+
+      try {
+        // const delegate = await delegateToMyself()
+      } catch (e) {
+        console.log('delegate error:', e)
+      }
+      try {
+        console.log('voting Yes...')
+
+        const gov = new ethers.Contract(govContract.address, govContract.abi, signer)
+        console.log('gov:', gov)
+        await gov.castVote(router.query.proposalId, 0)
+        await getCurrentVotes(router.query.proposalId)
+        setAgainstVotes(againstVotes + 1)
+        await getState(proposalId)
+        toast({
+          title: 'Voted!',
+          position: 'bottom',
+          description: 'Thank you for voting this proposal.',
+          status: 'success',
+          variant: 'subtle',
+          duration: 5000,
+          isClosable: true,
+        })
+      } catch (e: any) {
+        console.log('vote error:', e)
+        toast({
+          title: 'Error',
+          position: 'bottom',
+          description: "You can't vote twice",
+          status: 'error',
+          variant: 'subtle',
+          duration: 3000,
+          isClosable: true,
+        })
+      }
+      // setIsLoading(false)
+    } else {
+      console.log('no provider')
+      toast({
+        title: 'Disconnected',
+        position: 'bottom',
+        description: 'Please connect your wallet first.',
+        status: 'info',
+        variant: 'subtle',
+        duration: 2000,
+        isClosable: true,
+      })
+      // setIsLoading(false)
+      return
+    }
   }
 
   const execute = async () => {
     console.log('executing...')
-    // setIsLoading(true)
-    // try {
-    //   const targetsFormatted = [targets]
-    //   const valuesFormatted = [values]
-    //   const calldatasFormatted = [calldatas]
-    //   const hashedDescription = ethers.id(rawDescription)
+    let signer
+    if (provider) {
+      setIsLoading(true)
+      const ethersProvider = new BrowserProvider(provider)
+      signer = await ethersProvider.getSigner()
+      try {
+        const targetsFormatted = [targets]
+        const valuesFormatted = [values]
+        const calldatasFormatted = [calldatas]
+        const hashedDescription = ethers.id(rawDescription)
 
-    //   const gov = new ethers.Contract(GOV_CONTRACT_ADDRESS, GOV_CONTRACT_ABI, signer)
-    //   const executeCall = await gov.execute(targetsFormatted, valuesFormatted, calldatasFormatted, hashedDescription)
-    //   await executeCall.wait(1)
-    //   getState(proposalId)
-    //   setIsLoading(false)
-    // } catch (e: any) {
-    //   console.log('execute error:', e)
-    //   setIsLoading(false)
+        const gov = new ethers.Contract(govContract.address, govContract.abi, signer)
+        const executeCall = await gov.execute(targetsFormatted, valuesFormatted, calldatasFormatted, hashedDescription)
+        await executeCall.wait(1)
+        getState(proposalId)
+        setIsLoading(false)
+      } catch (e: any) {
+        console.log('execute error:', e)
+        setIsLoading(false)
 
-    //   toast({
-    //     title: 'Error',
-    //     position: 'bottom',
-    //     description: e?.data.message,
-    //     status: 'info',
-    //     variant: 'subtle',
-    //     duration: 3000,
-    //     isClosable: true,
-    //   })
-    // }
+        toast({
+          title: 'Error',
+          position: 'bottom',
+          description: e?.data.message,
+          status: 'info',
+          variant: 'subtle',
+          duration: 3000,
+          isClosable: true,
+        })
+      }
+    }
   }
-
   return initialized ? (
     <>
       <Head title={title} />
@@ -336,32 +366,33 @@ export default function Home() {
 
           <br />
         </div>
-
-        {/* const proposalState = ['Pending', 'Active', 'Canceled', 'Defeated', 'Succeeded', 'Queued', 'Expired', 'Executed'] */}
-        <Box mt={10} borderRadius="lg" p={5} shadow="md" borderWidth="2px">
-          <HeadingComponent as="h4">Are you in favor of this proposal?</HeadingComponent>
-          <br />
-          <Button mr="5" colorScheme="green" variant="outline" onClick={voteYes}>
-            Yes
-          </Button>
-          <Button colorScheme="red" variant="outline" onClick={voteNo}>
-            No
-          </Button>
-        </Box>
-
+        {state === 'Active' && (
+          <Box mt={10} borderRadius="lg" p={5} shadow="md" borderWidth="2px">
+            <HeadingComponent as="h4">Are you in favor of this proposal?</HeadingComponent>
+            <br />
+            <Button mr="5" colorScheme="green" variant="outline" onClick={voteYes}>
+              Yes
+            </Button>
+            <Button colorScheme="red" variant="outline" onClick={voteNo}>
+              No
+            </Button>
+          </Box>
+        )}
         {state === 'Succeeded' && (
           <>
             <br />
 
-            {!isLoading ? (
-              <Button size="xs" mt={5} colorScheme="red" variant="solid" onClick={execute}>
-                Execute
-              </Button>
-            ) : (
-              <Button isLoading loadingText="Executing..." size="xs" mt={5} colorScheme="red" variant="solid" onClick={execute}>
-                Execute
-              </Button>
-            )}
+            <Button
+              size="xs"
+              mt={5}
+              colorScheme="red"
+              variant="solid"
+              onClick={execute}
+              isLoading={isLoading}
+              loadingText="Executing..."
+              spinnerPlacement="end">
+              Execute
+            </Button>
           </>
         )}
       </main>
