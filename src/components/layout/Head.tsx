d4d8e0ff080ea78cbe23cc3f75cfb2a878d7e70f
julienbrg <julien@strat.cc>
2024-06-13 23:29:29 +0200

    fix head

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -11,30 +11,27 @@ export function Head(props: Props) {
   return (
     <NextHead>
       <title>{props.title ?? SITE_NAME} </title>
-      <meta property="og:url" content="https://gov-ui.netlify.app/" />
+      {/* <meta property="og:url" content="https://gov-ui.netlify.app/" /> */}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={props.title ?? SITE_NAME} />
       <meta name="twitter:card" content={SITE_DESCRIPTION} />
       <meta property="og:description" content={props.description ?? SITE_DESCRIPTION} />
-      <meta property="og:image" content={'./huangshan.png'} />
+      <meta property="og:image" content={'https://deploy-preview-84--gov-ui.netlify.app/huangshan.png'} />
       <meta name="description" content={props.description ?? SITE_DESCRIPTION} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      {/* <title></title>
-      <meta name="description" content="" />
-
       <meta property="og:url" content="https://deploy-preview-84--gov-ui.netlify.app" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="" />
-      <meta property="og:description" content="" />
-      <meta property="og:image" content="" />
+      {/* <meta property="og:title" content="" /> */}
+      {/* <meta property="og:description" content="" /> */}
+      {/* <meta property="og:image" content="" /> */}
 
-      <meta name="twitter:card" content="summary_large_image" />
+      {/* <meta name="twitter:card" content="summary_large_image" /> */}
       <meta property="twitter:domain" content="deploy-preview-84--gov-ui.netlify.app" />
       <meta property="twitter:url" content="https://deploy-preview-84--gov-ui.netlify.app" />
-      <meta name="twitter:title" content="" />
-      <meta name="twitter:description" content="" />
-      <meta name="twitter:image" content="" /> */}
+      <meta name="twitter:title" content={props.title ?? SITE_NAME} />
+      <meta name="twitter:description" content={props.description ?? SITE_DESCRIPTION} />
+      <meta name="twitter:image" content="https://deploy-preview-84--gov-ui.netlify.app/huangshan.png" />
     </NextHead>
   )
 }
