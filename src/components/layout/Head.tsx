dc05de5f84208662736e9198dd6ad8250a811186
julienbrg <julien@strat.cc>
2024-06-13 17:05:44 +0200

    fix link

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -16,9 +16,25 @@ export function Head(props: Props) {
       <meta property="og:title" content={props.title ?? SITE_NAME} />
       <meta name="twitter:card" content={SITE_DESCRIPTION} />
       <meta property="og:description" content={props.description ?? SITE_DESCRIPTION} />
-      <meta property="og:image" content={'./public/huangshan.png'} />
+      <meta property="og:image" content={'./huangshan.png'} />
       <meta name="description" content={props.description ?? SITE_DESCRIPTION} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      {/* <title></title>
+      <meta name="description" content="" />
+
+      <meta property="og:url" content="https://deploy-preview-84--gov-ui.netlify.app" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="" />
+      <meta property="og:description" content="" />
+      <meta property="og:image" content="" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="deploy-preview-84--gov-ui.netlify.app" />
+      <meta property="twitter:url" content="https://deploy-preview-84--gov-ui.netlify.app" />
+      <meta name="twitter:title" content="" />
+      <meta name="twitter:description" content="" />
+      <meta name="twitter:image" content="" /> */}
     </NextHead>
   )
 }
