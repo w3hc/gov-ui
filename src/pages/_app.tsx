84c722f1040a4151f8347eaf9f45063bed55857f
julienbrg <julien@strat.cc>
2024-06-14 12:04:32 +0200

    clean up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { Seo } from '../components/layout/Seo'
 import { useIsMounted } from '../hooks/useIsMounted'
 
-// https://github.com/wslyvh/nexth/blob/chakra/src/pages/_app.tsx
-// https://socialsharepreview.com/
-// https://www.opengraph.xyz/
-
 export default function App({ Component, pageProps }: AppProps) {
   const isMounted = useIsMounted()
 
