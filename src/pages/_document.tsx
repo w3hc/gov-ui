cd7271aeb08ed893961b9b187d3c77d90ff0a319
julienbrg <julien@strat.cc>
2024-06-14 11:20:00 +0200

    fix document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,13 @@
-import { Html, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript } from '@chakra-ui/react'
-import { Head } from '../components/layout/Head'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={'dark'} />
         <Main />
